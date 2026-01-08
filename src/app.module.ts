522480a2f295e81a7ba2ec3997f5636fdb56937c
ldjokokouam <lea.djoko-kouam@etudiant.univ-rennes1.fr>
2026-01-08 23:01:37 +0100

    Backend remarche

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,20 +7,26 @@ import { RecordModule } from './record/record.module';
 import { PlayerController } from './player/player.controller';
 import { RecordController } from './record/record.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PlayerService } from './player/player.service';
+import { ConfigModule } from '@nestjs/config'; 
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }), 
+
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
-      ssl: false,
+      ssl: {
+        rejectUnauthorized: false,
+      },
       synchronize: true,
       autoLoadEntities: true,
     }),
-    DemoModule, 
-    PlayerModule, 
-    RecordModule],
+
+    DemoModule,
+    PlayerModule,
+    RecordModule,
+  ],
   controllers: [AppController, PlayerController, RecordController],
   providers: [AppService],
 })
